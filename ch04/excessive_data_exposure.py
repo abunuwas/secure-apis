f1032ef66c698768f652bad4d81df16baf94157d
Jose Haro <joseharoperalta@gmail.com>
2025-06-20 07:37:40 +0100

    Update models and add model to set user up

--- ch04/excessive_data_exposure.py
+++ ch04/excessive_data_exposure.py
@@ -2,14 +2,16 @@ import uuid
 
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
-from sqlalchemy import Uuid, create_engine, select
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+
+from auth import validate_access, UserClaims
 
 server = FastAPI()
 
 
 class Base(DeclarativeBase):
-    id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, default=uuid.uuid4)
+    id: Mapped[str] = mapped_column(str, primary_key=True, default=uuid.uuid4)
 
 
 class ProjectModel(Base):
@@ -21,14 +23,16 @@ class ProjectModel(Base):
 class UserModel(Base):
     __tablename__ = "user"
 
+    id: Mapped[str]
+    full_name: Mapped[str]
     email: Mapped[str]
     project: Mapped[str]
-    full_name: Mapped[str]
 
 
 class GetUserProfileSchema(BaseModel):
-    email: str
+    id: str
     full_name: str
+    email: str
     project: str
 
 
@@ -52,29 +56,27 @@ with session_maker() as session:
         session.commit()
 
 
-class UserClaims(BaseModel):
-    sub: str
+class SetMeUp(BaseModel):
     project: str
+    full_name: str
+    email: str
 
 
-def validate_access():
-    return UserClaims(sub=str(uuid.uuid4()), project="Matrix")
-
-
-@server.get(
-    "/query-user-vulnerable",
-    response_model=GetUserProfileSchema,
-)
-async def query_user_vulnerable(
-    email: str, user_claims: UserClaims = Depends(validate_access)
+@server.put("/set-me-up", response_model=UserModel)
+def set_me_up(
+    user_details: SetMeUp, user_claims: UserClaims = Depends(validate_access)
 ):
     with session_maker() as session:
-        user = session.scalar(select(UserModel).where(UserModel.email == email))
-        if user is None:
-            raise HTTPException(
-                status_code=404, detail=f"User with email {email} not found."
-            )
+        user = UserModel(
+            id=user_claims.sub,
+            project=user_details.project,
+            full_name=user_details.full_name,
+            email=user_details.email,
+        )
+        session.add(user)
+        session.commit()
         return {
+            "id": user.id,
             "email": user.email,
             "project": user.project,
             "full_name": user.full_name,
@@ -82,26 +84,50 @@ async def query_user_vulnerable(
 
 
 @server.get(
-    "/query-user-safe",
+    "/vulnerable/query-user",
     response_model=GetUserProfileSchema,
 )
-async def query_user_safe(
+async def vulnerable_query_user(
     email: str, user_claims: UserClaims = Depends(validate_access)
 ):
     with session_maker() as session:
-        user = session.scalar(
-            select(UserModel).where(
-                UserModel.email == email,
-                UserModel.project == user_claims.project,
-            )
+        requested_user = session.scalar(
+            select(UserModel).where(UserModel.email == email)
         )
-        if user is None:
+        if requested_user is None:
             raise HTTPException(
                 status_code=404, detail=f"User with email {email} not found."
             )
         return {
-            "id": user.id,
-            "email": user.email,
-            "project": user.project,
-            "full_name": user.full_name,
+            "id": requested_user.id,
+            "email": requested_user.email,
+            "project": requested_user.project,
+            "full_name": requested_user.full_name,
+        }
+
+
+@server.get(
+    "/query-user",
+    response_model=GetUserProfileSchema,
+)
+async def query_user(email: str, user_claims: UserClaims = Depends(validate_access)):
+    with session_maker() as session:
+        requesting_user = session.scalar(
+            UserModel.id == user_claims.sub,
+        )
+        requested_user = session.scalar(
+            select(UserModel).where(
+                UserModel.email == email,
+                UserModel.project == requesting_user.project,
+            )
+        )
+        if requested_user is None:
+            raise HTTPException(
+                status_code=404, detail=f"User with email {email} not found."
+            )
+        return {
+            "id": requested_user.id,
+            "email": requested_user.email,
+            "project": requested_user.project,
+            "full_name": requested_user.full_name,
         }
